f2c209ba98b9bf4f22ac7285d960135192080919
since94k <since94k@gmail.com>
2023-06-16 02:00:38 +0900

    adjusting

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,7 @@
+var store = [{
+        "title": "make my first blog",
+        "excerpt":"Construst The Great Library  I have made this blog for the first in my life   this blog will be a Library to make post (Coding, Mathmatics, Travle, etc..)   thank you for visiting my Library     ","categories": ["Daily Life"],
+        "tags": [],
+        "url": "/daily%20life/first/",
+        "teaser": null
+      }]
