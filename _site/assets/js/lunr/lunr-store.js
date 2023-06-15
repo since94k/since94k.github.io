df22e912b135d486c6c1009204b713139596db3a
since94k <since94k@gmail.com>
2023-06-16 02:45:56 +0900

    add post

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -4,4 +4,10 @@ var store = [{
         "tags": [],
         "url": "/daily%20life/first/",
         "teaser": null
+      },{
+        "title": null,
+        "excerpt":"What is Jekyll Blog? Jekyll은 정적 웹사이트 생성기로 Ruby언어로 제작되었으며 마크다운 글쓰기를 제공한다. GitHub Pages는 Jekyll로 구동되기 때문에 내가 원하는 사이트를 GitHub를 통해 구현할 수 있다. 원하는대로 Customizing이 가능하고 다양한 테마를 사용할 수 있다. Step 1 Install a Homebrew Homebrew는 Mac OS에서 불가능한 것들을 할 수 있도록 한다. Max Howell이...","categories": [],
+        "tags": null,
+        "url": "/2023-06-16-first/",
+        "teaser": null
       }]
